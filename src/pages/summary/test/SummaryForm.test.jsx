282c15f4e40a51b4bb73cb743e8454dac14d6a6f
Marshall <danielm22@hotmail.co.uk>
2021-08-18 13:04:32 +0100

    test written: initial state of button / checkbox, disabling action

---
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -0,0 +1,25 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import SummaryForm from '../SummaryForm';
+
+test('initial checks', () => {
+  render(<SummaryForm />);
+  const confirmButton = screen.getByRole('button', { name: /confirm button/i });
+  const checkBox = screen.getByRole('checkbox', { name: 'Terms and Conditions' });
+
+  expect(checkBox).not.toBeChecked();
+  expect(confirmButton).toBeDisabled();
+});
+
+test('checking checkbox enables button, and un-checking disables it once more', () => {
+  render(<SummaryForm />);
+  const confirmButton = screen.getByRole('button', { name: /confirm button/i });
+  const checkBox = screen.getByRole('checkbox', { name: 'I agree to Terms and Conditions' });
+
+  fireEvent.click(checkBox);
+  expect(checkBox).toBeChecked();
+  expect(confirmButton).toBeEnabled();
+
+  fireEvent.click(checkBox);
+  expect(checkBox).not.toBeChecked();
+  expect(confirmButton).toBeDisabled();
+});
